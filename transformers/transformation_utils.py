973cdc607d88052fbb9ebb85ce340205dd90075d
kheuer <koos@assheuer.de>
2025-06-04 14:24:14 +0200

    fix the ordering of transformations in the pipeline

--- transformers/transformation_utils.py
+++ transformers/transformation_utils.py
@@ -8,50 +8,59 @@ from config import MAX_EPOCHS, BATCH_SIZE, PATIENCE
 
 
 def get_transform_pipeline(params: dict) -> function:
-    # TODO: add 4th transformation and add ordering of transformations
     transformations = []
-    if params["USE_AUGMIX"]:
+    for fn in params["TRANSFORMATIONS_ORDER"]:
+        if fn == "Augmix" and params["USE_AUGMIX"]:
 
-        class ScaleTo255:
-            def __call__(self, x):
-                return (x * 255).clamp(0, 255).byte()
+            class ScaleTo255:
+                def __call__(self, x):
+                    return (x * 255).clamp(0, 255).byte()
 
-        class Normalize:
-            def __call__(self, x):
-                return x / 255.0
+            class Normalize:
+                def __call__(self, x):
+                    return x / 255.0
 
-        augmix = Compose(
-            [
-                ScaleTo255(),
-                AugMix(
-                    severity=params["SEVERITY"],
-                    mixture_width=params["MIXTURE_WIDTH"],
-                    chain_depth=params["CHAIN_DEPTH"],
-                    alpha=params["ALPHA"],
-                    all_ops=params["ALL_OPS"],
-                    interpolation=params["INTERPOLATION"],
-                ),
-                Normalize(),
-            ]
-        )
-        transformations.append(augmix)
+            augmix = Compose(
+                [
+                    ScaleTo255(),
+                    AugMix(
+                        severity=params["SEVERITY"],
+                        mixture_width=params["MIXTURE_WIDTH"],
+                        chain_depth=params["CHAIN_DEPTH"],
+                        alpha=params["ALPHA"],
+                        all_ops=params["ALL_OPS"],
+                        interpolation=params["INTERPOLATION"],
+                    ),
+                    Normalize(),
+                ]
+            )
+            transformations.append(augmix)
 
-    if params["USE_FOURIER"]:
-        fourier = transforms.Compose(
-            [FourierTransformer(square_size=params["SQUARE_SIZE"], eta=params["ETA"])]
-        )
-        transformations.append(fourier)
+        elif fn == "Fourier" and params["USE_FOURIER"]:
+            fourier = transforms.Compose(
+                [
+                    FourierTransformer(
+                        square_size=params["SQUARE_SIZE"], eta=params["ETA"]
+                    )
+                ]
+            )
+            transformations.append(fourier)
 
-    if params["USE_JIGSAW"]:
-        jigsaw = transforms.Compose(
-            [
-                JigsawTransform(
-                    min_grid_size=params["MIN_GRID_SIZE"],
-                    max_grid_size=params["MAX_GRID_SIZE"],
-                )
-            ]
-        )
-        transformations.append(jigsaw)
+        elif fn == "Jigsaw" == params["USE_JIGSAW"]:
+            jigsaw = transforms.Compose(
+                [
+                    JigsawTransform(
+                        min_grid_size=params["MIN_GRID_SIZE"],
+                        max_grid_size=params["MAX_GRID_SIZE"],
+                    )
+                ]
+            )
+            transformations.append(jigsaw)
+
+        elif fn == "Dlow" and params["USE_DLOW"]:
+            # TODO: add the 4th transformation here once it is done
+            pass
+
     # no transformations are activated
     if not transformations:
         return lambda x: x
