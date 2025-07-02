5b8f4231997eeaa6b0900ba2cc1cd647297ab5a5
Koos Assheuer <koos@assheuer.de>
2025-07-02 10:57:51 +0200

    move params out

--- run_all_experiments.py
+++ run_all_experiments.py
@@ -15,6 +15,16 @@ from models import (
 from dataset_utils import all_datasets, seed
 from utils import split_df_into_loaders
 from transformers.transformation_utils import get_transform_pipeline
+from config import (
+    params_resnet_18_pretrained,
+    params_resnet_18_random,
+    params_resnet_18_pretrained_augmented,
+    params_resnet_18_random_augmented,
+    params_resnet_50_pretrained,
+    params_resnet_50_random,
+    params_resnet_50_pretrained_augmented,
+    params_resnet_50_random_augmented,
+)
 
 
 filename = "final_results.txt"
@@ -37,175 +47,6 @@ with open(filename, "w") as f:
     f.write(f"RANDOM SEED: {seed}")
 
 
-params_resnet_18_random = {
-    "EPOCHS": 300,
-    "PATIENCE": 50,
-    "LEARNING_RATE": 0.001,
-    "BETA_1": 0.9,
-    "BETA_2": 0.999,
-    "OPTIMIZER": "SGD",
-    "SCHEDULER": "CosineAnnealingLR",
-    "MOMENTUM": 0.53,
-    "DAMPENING": 0.0145,
-    "WEIGHT_DECAY": 0.0,
-    # augmentation params
-    "TRANSFORMATIONS_ORDER": [],
-}
-
-params_resnet_18_pretrained = {
-    "EPOCHS": 300,
-    "PATIENCE": 50,
-    "LEARNING_RATE": 0.0037,
-    "BETA_1": 0.95,
-    "BETA_2": 0.9999,
-    "OPTIMIZER": "SGD",
-    "SCHEDULER": "CosineAnnealingLR",
-    "MOMENTUM": 0.736,
-    "DAMPENING": 0.0327,
-    "WEIGHT_DECAY": 0.0068,
-    # augmentation params
-    "TRANSFORMATIONS_ORDER": [],
-}
-
-
-params_resnet_50_pretrained = {
-    "EPOCHS": 300,
-    "PATIENCE": 50,
-    "LEARNING_RATE": 0.0059,
-    "BETA_1": 0.8,
-    "BETA_2": 0.99,
-    "WEIGHT_DECAY": 0.0068,
-    "OPTIMIZER": "SGD",
-    "SCHEDULER": "StepLR",
-    "MOMENTUM": 0.6991,
-    "DAMPENING": 0.0713,
-    "GAMMA": 0.40,
-    "STEP_SIZE": 29,
-    # augmentation params
-    "TRANSFORMATIONS_ORDER": [],
-}
-
-params_resnet_50_random = {
-    "EPOCHS": 300,
-    "PATIENCE": 50,
-    "LEARNING_RATE": 0.0085,
-    "BETA_1": 0.95,
-    "BETA_2": 0.999,
-    "WEIGHT_DECAY": 0.08,
-    "OPTIMIZER": "SGD",
-    "SCHEDULER": "LinearLR",
-    "MOMENTUM": 0.81,
-    "DAMPENING": 0.17,
-    "GAMMA": 0.17,
-    "STEP_SIZE": 47,
-    # augmentation params
-    "TRANSFORMATIONS_ORDER": [],
-}
-
-params_resnet_18_random_augmented = {
-    "LEARNING_RATE": 0.005183947105738152,
-    "BETA_1": 0.9,
-    "BETA_2": 0.9999,
-    "WEIGHT_DECAY": 0.0805782454221565,
-    "OPTIMIZER": "AdamW",
-    "SCHEDULER": "CosineAnnealingLR",
-    "MOMENTUM": 0.60506162595354,
-    "DAMPENING": 0.17600007691402403,
-    "GAMMA": 0.5446314874569377,
-    "STEP_SIZE": 24,
-    # Augmix
-    "USE_AUGMIX": True,
-    "SEVERITY": 1,
-    "MIXTURE_WIDTH": 7,
-    "CHAIN_DEPTH": 7,
-    "ALPHA": 0.7480807243456533,
-    "ALL_OPS": True,
-    "INTERPOLATION": "NEAREST",
-    # Fourier
-    "USE_FOURIER": False,
-    # Jigsaw
-    "USE_JIGSAW": False,
-    # Dlow
-    "USE_DLOW": True,
-    "TRANSFORMATIONS_ORDER": "Dlow,Augmix",
-}
-
-
-params_resnet_50_random_augmented = {
-    "LEARNING_RATE": 0.007182152751327874,
-    "BETA_1": 0.95,
-    "BETA_2": 0.9999,
-    "WEIGHT_DECAY": 0.06805670931784548,
-    "OPTIMIZER": "AdamW",
-    "SCHEDULER": "None",
-    "MOMENTUM": 0.8028034706965037,
-    "DAMPENING": 0.05245600588394587,
-    "GAMMA": 0.8567303060327642,
-    "STEP_SIZE": 17,
-    # Augmix
-    "USE_AUGMIX": False,
-    # Fourier
-    "USE_FOURIER": True,
-    "SQUARE_SIZE_SINGLE_SIDE": 72,
-    "ETA": 0.4767002143366463,
-    # Jigsaw
-    "USE_JIGSAW": False,
-    # Dlow
-    "USE_DLOW": False,
-    "TRANSFORMATIONS_ORDER": "Augmix,Dlow,Fourier,Jigsaw",
-}
-
-params_resnet_18_pretrained_augmented = {
-    "LEARNING_RATE": 0.0023299101226650293,
-    "BETA_1": 0.9,
-    "BETA_2": 0.999,
-    "WEIGHT_DECAY": 0.030189290982594354,
-    "OPTIMIZER": "SGD",
-    "SCHEDULER": "LinearLR",
-    "MOMENTUM": 0.5298043637566164,
-    "DAMPENING": 0.17547013593977853,
-    "GAMMA": 0.3085666392448091,
-    "STEP_SIZE": 33,
-    # Augmix
-    "USE_AUGMIX": False,
-    # Fourier
-    "USE_FOURIER": False,
-    # Jigsaw
-    "USE_JIGSAW": False,
-    # Dlow
-    "USE_DLOW": False,
-    "TRANSFORMATIONS_ORDER": "Dlow,Augmix,Fourier,Jigsaw",
-}
-
-params_resnet_50_pretrained_augmented = {
-    "LEARNING_RATE": 0.001775604307448849,
-    "BETA_1": 0.95,
-    "BETA_2": 0.9999,
-    "WEIGHT_DECAY": 0.043133835070306284,
-    "OPTIMIZER": "SGD",
-    "SCHEDULER": "ReduceLROnPlateau",
-    "MOMENTUM": 0.5790938662197538,
-    "DAMPENING": 0.16106357162713794,
-    "GAMMA": 0.8997127883716795,
-    "STEP_SIZE": 7,
-    # Augmix
-    "USE_AUGMIX": True,
-    "SEVERITY": 5,
-    "MIXTURE_WIDTH": 2,
-    "CHAIN_DEPTH": 2,
-    "ALPHA": 0.7216280594498236,
-    "ALL_OPS": True,
-    "INTERPOLATION": "BILINEAR",
-    # Fourier
-    "USE_FOURIER": False,
-    # Jigsaw
-    "USE_JIGSAW": False,
-    # Dlow
-    "USE_DLOW": False,
-    "TRANSFORMATIONS_ORDER": "Augmix,Dlow,Fourier,Jigsaw",
-}
-
-
 dataset_name = "pacs"
 dataset = all_datasets[dataset_name]
 
@@ -224,30 +65,39 @@ def log(msg):
 
 N_RUNS = 4
 
-for model_name, pretrained, target_domain in tqdm(
-    product(model_names, pretrained_options, target_domains),
+for augmented, model_name, pretrained, target_domain in tqdm(
+    product(augmentations, model_names, pretrained_options, target_domains),
     total=len(model_names) * len(pretrained_options) * len(target_domains),
     desc="Running combinations",
 ):
+    print(
+        "augmented:",
+        augmented,
+        "model_name:",
+        model_name,
+        "pretrained:",
+        pretrained,
+        "target-domain:",
+        target_domain,
+    )
     train_loader, val_loader, test_loader, _ = split_df_into_loaders(
         df=dataset["df"], target_domain=target_domain
     )
-
     losses = []
     accuracies = []
     for i in range(N_RUNS):
         if model_name == "ResNet18":
             model = get_resnet_18(pretrained=pretrained)
-            if pretrained:
-                params = deepcopy(params_resnet_18_pretrained)
-            else:
-                params = deepcopy(params_resnet_18_random)
+            name = "resnet_18"
         elif model_name == "ResNet50":
             model = get_resnet_50(pretrained=pretrained)
-            if pretrained:
-                params = deepcopy(params_resnet_50_pretrained)
-            else:
-                params = deepcopy(params_resnet_50_random)
+            name = "resnet_50"
+
+        variable_name = f"params_{name}_{'pretrained' if pretrained else 'random'}"
+        if augmented:
+            variable_name += "_augmented"
+
+        params = deepcopy(globals().get(variable_name))
         params["TARGET_DOMAIN"] = target_domain
 
         _, weights = calculate_val_loss(
@@ -289,4 +139,6 @@ for model_name, pretrained, target_domain in tqdm(
     )
 
 # create the csv file with results
-import validate_results
+from validate_results import main
+
+main()
--- validate_results.py
+++ validate_results.py
@@ -20,50 +20,57 @@ builder = {
     "i": [],
 }
 
-for filename in tqdm(os.listdir("weights")):
-    architecture, target_domain, pretrained, augmented, i = filename.replace(
-        "art_painting", "art-painting"
-    ).split("_")
-    i = i.removesuffix(".pth")
-    if target_domain == "art-painting":
-        target_domain = "art_painting"
-    pretrained = pretrained.startswith("True")
-    augmented = augmented.startswith("True")
-    if architecture == "ResNet18":
-        model = get_resnet_18(pretrained=False)
-    else:
-        model = get_resnet_50(pretrained=False)
 
-    state_dict = load(os.path.join("weights", filename), map_location=device)
-    model.load_state_dict(state_dict)
-    model.to(device)
+def main():
+    for filename in tqdm(os.listdir("weights")):
+        architecture, target_domain, pretrained, augmented, i = filename.replace(
+            "art_painting", "art-painting"
+        ).split("_")
+        i = i.removesuffix(".pth")
+        if target_domain == "art-painting":
+            target_domain = "art_painting"
+        pretrained = pretrained.startswith("True")
+        augmented = augmented.startswith("True")
+        if architecture == "ResNet18":
+            model = get_resnet_18(pretrained=False)
+        else:
+            model = get_resnet_50(pretrained=False)
 
-    train_loader, _, test_loader, _ = split_df_into_loaders(
-        df=dataset["df"], target_domain=target_domain
-    )
+        state_dict = load(os.path.join("weights", filename), map_location=device)
+        model.load_state_dict(state_dict)
+        model.to(device)
 
-    transformation_pipeline = get_transform_pipeline(
-        params={
-            "TRANSFORMATIONS_ORDER": "",
-        }
-    )
+        train_loader, _, test_loader, _ = split_df_into_loaders(
+            df=dataset["df"], target_domain=target_domain
+        )
 
-    builder["taget_domain"].append(target_domain)
-    builder["architecture"].append(architecture)
-    builder["pretrained"].append(pretrained)
-    builder["i"].append(i)
+        transformation_pipeline = get_transform_pipeline(
+            params={
+                "TRANSFORMATIONS_ORDER": "",
+            }
+        )
 
-    loss, accuracy = _do_eval(
-        model=model,
-        criterion=get_criterion(train_loader),
-        dataloader=test_loader,
-        transformation_pipeline=transformation_pipeline,
-    )
-    builder["test_loss"].append(loss)
-    builder["test_accuracy"].append(accuracy)
+        builder["taget_domain"].append(target_domain)
+        builder["architecture"].append(architecture)
+        builder["pretrained"].append(pretrained)
+        builder["i"].append(i)
 
-    del model
+        loss, accuracy = _do_eval(
+            model=model,
+            criterion=get_criterion(train_loader),
+            dataloader=test_loader,
+            transformation_pipeline=transformation_pipeline,
+        )
+        builder["test_loss"].append(loss)
+        builder["test_accuracy"].append(accuracy)
 
-df = pd.DataFrame(builder)
-df.to_csv("results.csv")
-print(df)
+        del model
+
+    df = pd.DataFrame(builder)
+    df.to_csv("results.csv")
+    print(df)
+    return df
+
+
+if __name__ == "__main__":
+    main()
