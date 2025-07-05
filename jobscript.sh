a520facb15289b92dceac53be0fd69c233528f1f
danielWuertigner <danielwpunkt86@gmail.com>
2025-07-05 14:22:55 +0200

    updated jobscript

--- jobscript.sh
+++ jobscript.sh
@@ -14,6 +14,6 @@ tar xvzf "$WORK/camelyon17.tar.gz" -C "$TMPDIR"
 
 #copy .tar to local ssd and unzip is done in script
 #copy only checkpoints to $work during running
-python3 /home/woody/barz/barz129h/xai_proj_m/tuner.py
+python3 /home/woody/barz/barz129h/xai_proj_m/tuner.py --model ResNet18 --pretrained True --transformations False --targetdomain 0
 
 cp -r "$TMPDIR/trials" "$WORK"
