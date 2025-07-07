ab617cb76be5a232795357e97be3cf45d208740e
danielWuertigner <danielwpunkt86@gmail.com>
2025-07-07 17:52:03 +0200

    updated jobscript

---
+++ jobs.sh
@@ -0,0 +1,10 @@
+python3 /home/woody/barz/barz129h/xai_proj_m/tuner.py --model ResNet18 --pretrained True --transformations False --targetdomain 3
+
+python3 /home/woody/barz/barz129h/xai_proj_m/tuner.py --model ResNet18 --pretrained False --transformations False --targetdomain 0
+
+python3 /home/woody/barz/barz129h/xai_proj_m/tuner.py --model ResNet18 --pretrained False --transformations False --targetdomain 1
+
+python3 /home/woody/barz/barz129h/xai_proj_m/tuner.py --model ResNet18 --pretrained False --transformations False --targetdomain 2
+
+python3 /home/woody/barz/barz129h/xai_proj_m/tuner.py --model ResNet18 --pretrained False --transformations False --targetdomain 3
+
--- jobscript.sh
+++ jobscript.sh
@@ -11,7 +11,7 @@ module add python
 conda activate xAI-Proj
 
 tar xf "$WORK/camelyon17.tar.gz" -C "$TMPDIR"
-
+cp "$WORK/xai_proj_m/resnet18.pth" "$TMPDIR"
 #copy .tar to local ssd and unzip is done in script
 #copy only checkpoints to $work during running
 #python3 /home/woody/barz/barz129h/xai_proj_m/tuner.py --model ResNet18 --pretrained True --transformations False --targetdomain 0
