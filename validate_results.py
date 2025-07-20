bugfix auc-score computation

validate_results.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import gc
 from copy import deepcopy
@@ -18,6 +19,14 @@ from train_for_comparision_study import params_list
 from config import DEFAULT_PARAMS
 
 dataset_name = "camelyon_unbalanced"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--dataset_name", required=False, choices=["pacs", "camelyon", "camelyon_unbalanced"])
+args = parser.parse_args()
+
+if args.dataset_name is not None:
+    dataset_name = args.dataset_name
+print(dataset_name)
 dataset = all_datasets[dataset_name]
 label_mean = dataset["df"]["labels"].mean()
 builder = {
@@ -83,11 +92,16 @@ def main():
         )
         if "camelyon" in dataset_name:
             probabilities = torch.softmax(torch.tensor(all_logits), dim=1)
-            auc_value = roc_auc_score(all_labels, probabilities[:, 0])
-            predicted = np.argmax(probabilities, axis=1)
+            # model learned to classify tumor as 0 and no tumor as 1
+            # swap labels for tumor = 1 and no tumor = 0
+            inverted_labels = np.array([1-y for y in all_labels])
+            auc_value = roc_auc_score(inverted_labels, probabilities[:, 0])
+
+            predicted = np.argmax(probabilities.numpy(), axis=1)
+            predicted_inverted = np.array([1-pred for pred in predicted])
             # model learned to classify tumor as 0 and no tumor as 1
             # swap values
-            tn, fp, fn, tp = confusion_matrix(all_labels, predicted, labels=[1,0]).ravel()
+            tn, fp, fn, tp = confusion_matrix(inverted_labels, predicted_inverted).ravel()
             builder["tp"].append(tp)
             builder["tn"].append(tn)
             builder["fp"].append(fp)
@@ -108,7 +122,7 @@ def main():
     empty_cache()
     df = pd.DataFrame(builder)
     df["pretrained"] = False
-    df.to_csv("results.csv")
+    df.to_csv(f"results_{dataset_name}.csv")
     print(df)
     return df
 